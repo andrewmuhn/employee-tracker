7b75fe8b27e7f675bd4d4e060137d0d59b540e9c
andrew <andrewmuhn@gmail.com>
2023-04-14 20:33:08 -0700

    feat: fixed schema and added seeds

--- db/schema.sql
+++ db/schema.sql
@@ -11,9 +11,9 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NOT,
+    salary DECIMAL(8, 0) NOT NULL,
     department_id INT NOT NULL,
-    FOREIGN KEY(department_id) REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -22,6 +22,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
-    FOREIGN KEY(role_id) REFERENCES role(id),
-    FOREIGN KEY(manager_id) REFERENCES employee(id)
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    ON DELETE SET NULL
 );
