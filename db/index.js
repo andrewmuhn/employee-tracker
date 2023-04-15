a768c5b8a1ac1f1cb738f1c49567118273beef30
andrew <andrewmuhn@gmail.com>
2023-04-14 22:10:36 -0700

    feat: initialized query functions

---
+++ db/index.js
@@ -0,0 +1,40 @@
+const db = require("../config/db");
+
+const getAllDepartments = (err, req, res) => {};
+
+const addDepartment = (err, req, res) => {};
+
+const updateDepartment = (err, req, res) => {};
+
+const deleteDepartment = (err, req, res) => {};
+
+const getAllRoles = (err, req, res) => {};
+
+const addRole = (err, req, res) => {};
+
+const updateRole = (err, req, res) => {};
+
+const deleteRole = (err, req, res) => {};
+
+const getAllEmployees = (err, req, res) => {};
+
+const addEmployee = (err, req, res) => {};
+
+const updateEmployee = (err, req, res) => {};
+
+const deleteEmployee = (err, req, res) => {};
+
+module.exports = {
+  getAllDepartments,
+  addDepartment,
+  updateDepartment,
+  deleteDepartment,
+  getAllRoles,
+  addRole,
+  updateRole,
+  deleteRole,
+  getAllEmployees,
+  addEmployee,
+  updateEmployee,
+  deleteEmployee,
+};
