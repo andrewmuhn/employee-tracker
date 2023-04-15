4d771fe132c148cfad81ceb2e1668d9e44774003
andrew <andrewmuhn@gmail.com>
2023-04-14 22:57:14 -0700

    feat:GET, Post, Update, DELETE requests department

--- db/index.js
+++ db/index.js
@@ -1,47 +1,83 @@
 const db = require("../config/db");
+const cTable = require("console.table");
 
 const getAllDepartments = (req, res) => {
-  const sql = "";
+  const sql = "SELECT id, name AS Name FROM department";
+
   db.promise()
     .query(sql)
     .then((results) => {
-      res.json({});
+      res.json({
+        message: "Get request for all departments",
+      });
+      console.table(results[0]);
     })
     .catch((err) => console.error(err));
 };
 
 const addDepartment = (req, res) => {
-  const sql = "";
+  const sql = "INSERT INTO department (name) VALUES (?)";
+  const param = req.body.name;
+
   db.promise()
-    .query(sql)
+    .query(sql, param)
     .then((results) => {
-      res.json({});
+      res.json({
+        message: "Post request to department",
+        added: req.body,
+      });
+      console.log(`Added ${param} to the database`);
     })
     .catch((err) => console.error(err));
 };
 
 const updateDepartment = (req, res) => {
-  const sql = "";
+  const sql = "UPDATE department SET name = ? WHERE id = ?";
+  const params = [req.body.name, req.params.id];
+
   db.promise()
-    .query(sql)
+    .query(sql, params)
     .then((results) => {
-      res.json({});
+      if (!results[0].affectedRows) {
+        res.json({
+          message: "Department not found",
+        });
+      } else {
+        res.json({
+          message: "Update request to department",
+          changed: req.body,
+        });
+        console.log(`Changed department id: ${params[1]} to ${params[0]}`);
+      }
     })
     .catch((err) => console.error(err));
 };
 
 const deleteDepartment = (req, res) => {
-  const sql = "";
+  const sql = "DELETE FROM department WHERE id = ?";
+  const param = req.params.id;
+
   db.promise()
-    .query(sql)
+    .query(sql, param)
     .then((results) => {
-      res.json({});
+      if (!results[0].affectedRows) {
+        res.json({
+          message: "Department not found",
+        });
+      } else {
+        res.json({
+          message: "Delete request to department",
+          deletion: req.body,
+        });
+        console.log(`Deleted department id: ${param}`);
+      }
     })
     .catch((err) => console.error(err));
 };
 
 const getAllRoles = (req, res) => {
   const sql = "";
+
   db.promise()
     .query(sql)
     .then((results) => {
@@ -52,6 +88,7 @@ const getAllRoles = (req, res) => {
 
 const addRole = (req, res) => {
   const sql = "";
+
   db.promise()
     .query(sql)
     .then((results) => {
@@ -62,6 +99,7 @@ const addRole = (req, res) => {
 
 const updateRole = (req, res) => {
   const sql = "";
+
   db.promise()
     .query(sql)
     .then((results) => {
@@ -72,6 +110,7 @@ const updateRole = (req, res) => {
 
 const deleteRole = (req, res) => {
   const sql = "";
+
   db.promise()
     .query(sql)
     .then((results) => {
@@ -82,6 +121,7 @@ const deleteRole = (req, res) => {
 
 const getAllEmployees = (req, res) => {
   const sql = "";
+
   db.promise()
     .query(sql)
     .then((results) => {
@@ -92,6 +132,7 @@ const getAllEmployees = (req, res) => {
 
 const addEmployee = (req, res) => {
   const sql = "";
+
   db.promise()
     .query(sql)
     .then((results) => {
@@ -102,6 +143,7 @@ const addEmployee = (req, res) => {
 
 const updateEmployee = (req, res) => {
   const sql = "";
+
   db.promise()
     .query(sql)
     .then((results) => {
@@ -112,6 +154,7 @@ const updateEmployee = (req, res) => {
 
 const deleteEmployee = (req, res) => {
   const sql = "";
+
   db.promise()
     .query(sql)
     .then((results) => {
---
+++ test.session.sql
@@ -0,0 +1,6 @@
+--  @block
+USE employee_db;
+
+
+-- @block
+DELETE FROM department WHERE id = 6;
