6747b6c8bc17172dcf0d106b48be5eeb0a0fd222
andrew <andrewmuhn@gmail.com>
2023-04-14 22:09:20 -0700

    feat: added routing  to call mySQL Functions

--- routes/employee.js
+++ routes/employee.js
@@ -1,3 +1,13 @@
 const router = require("express").Router();
+const {
+  getAllEmployees,
+  addEmployee,
+  updateEmployee,
+  deleteEmployee,
+} = require("../db");
+
+router.route("/").get(getAllEmployees).post(addEmployee);
+
+router.route("/:id").put(updateEmployee).delete(deleteEmployee);
 
 module.exports = router;
--- routes/role.js
+++ routes/role.js
@@ -1,3 +1,8 @@
 const router = require("express").Router();
+const { getAllRoles, addRole, updateRole, deleteRole } = require("../db");
+
+router.route("/").get(getAllRoles).post(addRole);
+
+router.route("/:id").put(updateRole).delete(deleteRole);
 
 module.exports = router;
