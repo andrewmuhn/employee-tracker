42d5b815ffab1bc30bf8491aac7dbc38b22d0a23
andrew <andrewmuhn@gmail.com>
2023-04-15 13:14:46 -0700

    feat: added modular db queries based off table

---
+++ db/department.js
@@ -0,0 +1,80 @@
+const db = require("../config/db");
+const cTable = require("console.table");
+
+module.exports = {
+  getAllDepartments(req, res) {
+    const sql = "SELECT id, name AS Name FROM department";
+
+    db.promise()
+      .query(sql)
+      .then((results) => {
+        res.json({
+          message: "Get request for all departments",
+        });
+        console.table(results[0]);
+      })
+      .catch((err) => console.error(err));
+  },
+
+  getTotalBudget(req, res) {},
+
+  addDepartment(req, res) {
+    const sql = "INSERT INTO department (name) VALUES (?)";
+    const param = req.body.name;
+
+    db.promise()
+      .query(sql, param)
+      .then((results) => {
+        res.json({
+          message: "Post request to department",
+          added: req.body,
+        });
+        console.log(`Added ${param} to the database`);
+      })
+      .catch((err) => console.error(err));
+  },
+
+  updateDepartment(req, res) {
+    const sql = "UPDATE department SET name = ? WHERE id = ?";
+    const params = [req.body.name, req.params.id];
+
+    db.promise()
+      .query(sql, params)
+      .then((results) => {
+        if (!results[0].affectedRows) {
+          res.json({
+            message: "Department not found",
+          });
+        } else {
+          res.json({
+            message: "Update request to department",
+            changed: req.body,
+          });
+          console.log(`Updated database with ${params[0]}`);
+        }
+      })
+      .catch((err) => console.error(err));
+  },
+
+  deleteDepartment(req, res) {
+    const sql = "DELETE FROM department WHERE id = ?";
+    const param = req.params.id;
+
+    db.promise()
+      .query(sql, param)
+      .then((results) => {
+        if (!results[0].affectedRows) {
+          res.json({
+            message: "Department not found",
+          });
+        } else {
+          res.json({
+            message: "Delete request to department",
+            deletion: req.body,
+          });
+          console.log(`Deleted a department from the database`);
+        }
+      })
+      .catch((err) => console.error(err));
+  },
+};
