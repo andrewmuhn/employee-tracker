4103f6578cf5e889937374a864d8a3d12d13f647
andrew <andrewmuhn@gmail.com>
2023-04-15 21:42:02 -0700

    feat: initialized cli and created boilerplate

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const api = require("./routes");
+const CLI = require("./lib");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,7 +10,7 @@ app.use(express.json());
 
 app.use("/api", api);
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   console.error(err.stack);
   console.error(err.name);
   console.error(err.code);
@@ -20,8 +21,12 @@ app.use((err, req, res) => {
   res.status(404).json({
     message: "Not Found: Check your URL",
   });
+  next();
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening at https://localhost:${PORT}`);
+  const cli = new CLI();
+  // cli.asciHeader();
+  cli.run();
 });
